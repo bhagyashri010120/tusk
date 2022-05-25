18e8e21bc190768f2042159b0ed68235838f67cd
bhagyashri010120 <106133998+bhagyashri010120@users.noreply.github.com>
2022-05-25 12:22:17 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3, b = 8, c;
+            c = a + b;
+            Console.WriteLine(c);
+        }
+    }
+}
